beb934907b1126ac01b98ac690cae7a2a6ba26f0
hosam dahab <hosamdahab778@gmail.com>
2023-03-21 19:34:19 +0200

    f

--- resources/views/main/layouts/navbar.blade.php
+++ resources/views/main/layouts/navbar.blade.php
@@ -280,26 +280,28 @@
           </div>
           {{-- chat body --}}
         <div class="card-body" id="CardBody">
-
-            @foreach ($user_messages as $user_message)
-                @if ($user_message->sender=='user')
-                <div class="d-flex flex-row justify-content-start mb-4">
-                
-                    <div class="p-3 ms-3" style="border-radius: 15px; background-color: rgba(57, 192, 237,.2);">
-                        <p class="small mb-0">{{ $user_message->message }}</p>
-                    </div>
-                </div>
-                @endif
-                @if ($user_message->sender=='admin')
-                    <div class="d-flex flex-row justify-content-end mb-4">
-                        <div class="p-3 me-3 border" style="border-radius: 15px; background-color: #fbfbfb;">
+            @if (isset($user_messages))
+                @foreach ($user_messages as $user_message)
+                    @if ($user_message->sender=='user')
+                    <div class="d-flex flex-row justify-content-start mb-4">
+                    
+                        <div class="p-3 ms-3" style="border-radius: 15px; background-color: rgba(57, 192, 237,.2);">
                             <p class="small mb-0">{{ $user_message->message }}</p>
                         </div>
                     </div>
-                @endif
+                    @endif
+                    @if ($user_message->sender=='admin')
+                        <div class="d-flex flex-row justify-content-end mb-4">
+                            <div class="p-3 me-3 border" style="border-radius: 15px; background-color: #fbfbfb;">
+                                <p class="small mb-0">{{ $user_message->message }}</p>
+                            </div>
+                        </div>
+                    @endif
 
-            
-            @endforeach
+                
+                @endforeach
+            @endif
+           
             
 
 
